81ad87dd6e2c1b73e0c043045c77e33e4dbbf605
acehinnnqru <acehinnnqru@gmail.com>
2024-06-02 19:40:53 +0800

    feat: done 02.01-09

--- exercises/02_basic_calculator/09_saturating/src/lib.rs
+++ exercises/02_basic_calculator/09_saturating/src/lib.rs
@@ -1,9 +1,14 @@
+use std::u32;
+
 pub fn factorial(n: u32) -> u32 {
-    let mut result = 1;
+    let mut result: u32 = 1;
     for i in 1..=n {
         // Use saturating multiplication to stop at the maximum value of u32
         // rather than overflowing and wrapping around
-        result *= i;
+        result = match result.checked_mul(i) {
+            Some(v) => v,
+            None => return u32::MAX,
+        };
     }
     result
 }
